fffd50b73199f9caadb096f2f1254e2e28d40245
Git-hit <ayanali8178@gmail.com>
2025-02-15 22:55:28 +0530

    Added webhook verify page

---
+++ src/pages/admin.jsx
@@ -0,0 +1,5 @@
+export default function VerifyWebhook(){
+    return(
+        <h1>Verify Webhook</h1>
+    )
+}
--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import Home from "./pages/home";
 import PrivacyPolicy from "./pages/privacy-policy";
 import TermsAndCo from "./pages/terms-and-co";
+import VerifyWebhook from "./pages/admin";
 
 export const router = createBrowserRouter([
     {
@@ -15,5 +16,9 @@ export const router = createBrowserRouter([
     {
         path: '/terms-and-co',
         element: <TermsAndCo />
+    },
+    {
+        path: '/verify-webhook',
+        element: <VerifyWebhook />
     }
 ])
